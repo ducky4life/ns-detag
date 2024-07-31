173fd8025c8959feb93240897951a187074d22a6
Ducky <124077142+ducky4life@users.noreply.github.com>
2024-07-31 13:28:30 +0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extra Detag Functionalities
 // @namespace    https://ducky4life.github.io/tgw
-// @version      1.2.1
+// @version      1.2.2
 // @description  meow
 // @author       Ducky
 // @match        *://*.nationstates.net/*
