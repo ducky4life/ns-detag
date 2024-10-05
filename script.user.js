818112b1a883e2eab071336762c923f3f590be2a
Ducky <124077142+ducky4life@users.noreply.github.com>
2024-10-06 07:08:00 +0800

    added useragent to comply with script rules

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extra Detag Functionalities
 // @namespace    https://ducky4life.github.io/tgw
-// @version      1.3.0
+// @version      1.3.1
 // @description  meow
 // @author       Ducky
 // @match        *://*.nationstates.net/*
@@ -9,13 +9,20 @@
 // ==/UserScript==
 
 
-// please set your keybinds/password/RO name here
-// you can change the keybinds by replacing the constant value with another key of your choice in the script, and replace 'detag' with your RO name/'password123' with your password for puppets :D
+
+// Configuration
+
+const version = "1_3_1"
+const main_nation_name = "" // IMPORTANT Please set your main nation name here to comply with the new script rules. The script will not work properly if you do not.
+const password = "" // your password for your puppets (all puppets must share the same password)
+const ROname = "detag" // replace 'detag' with your RO name
+
+// Keybinds
+
+// you can change the keybinds by replacing the constant value with another key of your choice in the script
 // for a list of key names, go to https://www.toptal.com/developers/keycode and copy the 'event.code' info!
 // github repo: https://github.com/ducky4life/ns-detag
 
-const ROname = "detag"
-const password = "password123" // your password for your puppets (all puppets must share the same password)
 const restoregovkey = "KeyL"
 const eyebeastkey = "KeyO"
 const toggletemplatekey = "KeyK"
@@ -25,15 +32,20 @@ const puppetloginkey = "KeyY"
 
 
 
-// some of the code here is modified from https://github.com/rootabeta/YAFFeather, thanks a lot!
+// Code
 
+// some of the code here is modified from https://github.com/rootabeta/YAFFeather, thanks a lot!
 // ignore my shit code
+
 const regionlink = window.location.href
 const reg = /(?<=.nationstates.net).*$/
 const domain = regionlink.replace(reg, "")
 const region = regionlink.replace(domain, "")
 const regionname = region.replace("/", "")
 const nationpage = regionname.replace("template-overall=none/", "")
+const main = main_nation_name.replaceAll(" ", "_")
+const useragent = "?script=ns_detag__v" + version + "__by_ducky__used_by_" + main + "&userclick=" + Date.now()
+
 
 // code i guess
 document.addEventListener("keyup", function (event) { // no spam
@@ -44,11 +56,11 @@ document.addEventListener("keyup", function (event) { // no spam
 
 
 
-                // restores gov name to original for detags
+                // restores gov name to default for detags
                 case restoregovkey:
 
                 // first button press takes you to https://www.nationstates.net/page=regional_officer/region/office=governor/ if you are not there
-                // second one replaces current gov name with the default one and immediately renames it, no submit button required
+                // second one replaces current gov name with the default one and immediately renames it
 
 				if (window.location.href.includes("office=governor")) { // rename
 					document.getElementsByName("office_name")[0].value = "Governor";
@@ -56,13 +68,13 @@ document.addEventListener("keyup", function (event) { // no spam
 				}
 
 				else { // go to gov page
-					window.location.assign(domain + "/page=regional_officer/region/office=governor/");
+					window.location.assign(domain + "/page=regional_officer/region/office=governor" + useragent);
 				}
 				break;
 
 
 
-                // goes to the eyebeast page of current region page (PLEASE ONLY CLICK THIS IF ON A REGION PAGE)
+                // goes to the eyebeast page of current region page
                 case eyebeastkey:
                 window.location.assign("https://eyebeast.calref.ca/?" + regionname);
                 break;
@@ -73,11 +85,11 @@ document.addEventListener("keyup", function (event) { // no spam
                 case toggletemplatekey:
 
                 if (window.location.href.includes("template-overall=none")) { // on none
-                window.location.href = document.URL.replace("template-overall=none/", "");
+                window.location.href = document.URL.replace("template-overall=none/", "") + useragent;
                 }
 
                 else { // on normal
-                window.location.href = `/template-overall=none${document.URL.replace(domain, "")}`;
+                window.location.href = `/template-overall=none${document.URL.replace(domain, "")}` + useragent;
                 }
                 break;
 
@@ -87,11 +99,11 @@ document.addEventListener("keyup", function (event) { // no spam
                 case togglefastkey:
 
                 if (window.location.href.includes("fast.nationstates.net")) { // on fast
-                window.location.href = document.URL.replace("fast.", "www.");
+                window.location.href = document.URL.replace("fast.", "www.") + useragent;
                 }
 
                 else { // on normal
-                window.location.href = document.URL.replace("www.", "fast.");
+                window.location.href = document.URL.replace("www.", "fast.") + useragent;
                 }
                 break;
 
@@ -109,18 +121,17 @@ document.addEventListener("keyup", function (event) { // no spam
 				}
 
 				else { // go to self RO page
-					window.location.assign(domain + "/page=regional_officer/nation=" + document.getElementById("loggedin").getAttribute("data-nname"));
+					window.location.assign(domain + "/page=regional_officer/nation=" + document.getElementById("loggedin").getAttribute("data-nname") + useragent);
 				}
                 break;
 
 
 
-		// go to wa page of the current nation page after loggin in with provided password
+		        // go to wa page of the current nation page after loggin in with provided password
                 case puppetloginkey:
 
-                window.location.assign(domain + "/page=un?" + nationpage + "&password=" + password + "&logging_in=1");
+                window.location.assign(domain + "/page=un" + useragent + "&" + nationpage + "&password=" + password + "&logging_in=1");
                 break;
-
 		}
 	}
 });
